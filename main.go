160d916a41dd20e5aae3463d1b6917127f2718b9
souzannatha <souzanatha04@gmail.com>
2023-06-07 16:49:16 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func ExibeTodosAlunos(c *gin.Context) {
+	c.JSON(200,gin.H{
+		"id":"1",
+		"nome": "Nathã Souza Lopes",
+	})
+
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/alunos", ExibeTodosAlunos)
+	r.Run()
+}
